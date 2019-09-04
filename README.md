c956c83dc8216bb864be8021774b17186bc21337
shijir0927 <shijiree_mng@yahoo.com>
2019-09-04 16:13:15 -0700

    finished all 4 commands and readme

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # liri-node-app
+
+
+## What the project does?
+
+LIRI is like iPhone’s SIRI. However, while SIRI is a Speech Interpretation and Recognition Interface, LIRI is a Language Interpretation and Recognition Interface. LIRI will be a command line node app that takes in parameters and gives you back data.
+
+It has 4 different commands:
+
+    1. concert-this
+    2. spotify-this-song
+    3. movie-this
+    4. do-what-it-says
+
+**concert-this** command lets you search concerts from [bandsintown.com](bandsintown.com)
+![example](./images/concert-this.PNG)
+
+**spotify-this-song** command lets you search a song from [Spotify](spotify.com)
+![example](./images/spotify-this-song.PNG)
+![example](./images/spotify-this-song-1.PNG)
+
+**movie-this** command lets you search a movie from [IMDB API](imdb.com)
+![example](./images/movie-this.PNG)
+![example](./images/movie-this-1.PNG)
+
+**do-what-it-says** command reads from a file names random.txt and does the command written in that text file
+![example](./images/do-what-it-says.PNG)
+
+
+In addition to logging the data to the terminal/bash window, we also output the data to a text file called log.txt.
+![log-text](./images/log-txt.PNG)
+
+## Why the project is useful?
+
+This project can be useful since the person can search 3 different types of entertainment things from the command line.
+
+*EX: If I was working on my work projects and figuring out pushing and pulling conflicts and suddenly feel like going to a concert, I can just search it up from my command line without having to go to browser.*
+
---
+++ liri.js
@@ -0,0 +1,196 @@
+require("dotenv").config();
+let moment = require("moment");
+let fs = require("fs");
+let axios = require("axios");
+let keys = require("./keys.js");
+let Spotify = require("node-spotify-api");
+let spotify = new Spotify(keys.spotify);
+
+let command = process.argv[2];
+let input = process.argv.slice(3).join(" ");
+
+function liriBot() {
+    switch (command) {
+        case "concert-this":
+            axios.get("https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp")
+                .then(function(response) {
+                    console.log("---------------------------------------------------------------")
+                    console.log(`The venue name is: ${response.data[0].venue.name}`)
+                    console.log(`The venue location is: ${response.data[0].venue.city}, ${response.data[0].venue.region}, ${response.data[0].venue.country}`)
+                    console.log(`The date of the event is: ${moment(response.data[0].datetime).format("MM/DD/YYYY")}`)
+                    console.log("---------------------------------------------------------------")
+
+                    let text = [`The venue name is: ${response.data[0].venue.name}`,
+                        `The venue location is: ${response.data[0].venue.city}, ${response.data[0].venue.region}, ${response.data[0].venue.country}`,
+                        `The date of the event is: ${moment(response.data[0].datetime).format("MM/DD/YYYY")}`
+                    ]
+
+                    fs.appendFile("log.txt", text, function(err) {
+
+                        // If an error was experienced we will log it.
+                        if (err) {
+                            console.log(err);
+                        }
+
+                    });
+
+                })
+            break;
+
+        case "spotify-this-song":
+
+            if (process.argv.length == 3) {
+                spotify.search({ type: 'track', query: "The Sign Ace of Base" }, function(err, data) {
+                    if (err) {
+                        return console.log('Error occurred: ' + err);
+                    }
+                    console.log("---------------------------------------------------------------")
+                    console.log(`The name of the artist is: ${data.tracks.items[0].artists[0].name}`);
+                    console.log(`The name of the song is: ${data.tracks.items[0].name}`);
+                    console.log(`The preview link to the song is: ${data.tracks.items[0].preview_url}`);
+                    console.log(`The album name of this song is: ${data.tracks.items[0].album.name}`);
+                    console.log("---------------------------------------------------------------")
+
+                    let text = [`The name of the artist is: ${data.tracks.items[0].artists[0].name}`,
+                        `The name of the song is: ${data.tracks.items[0].name}`,
+                        `The preview link to the song is: ${data.tracks.items[0].preview_url}`,
+                        `The album name of this song is: ${data.tracks.items[0].album.name}`
+                    ]
+
+                    fs.appendFile("log.txt", text, function(err) {
+
+                        // If an error was experienced we will log it.
+                        if (err) {
+                            console.log(err);
+                        }
+
+                    });
+
+                });
+            } else if (process.argv.length > 3) {
+                spotify.search({ type: 'track', query: input }, function(err, data) {
+                    if (err) {
+                        return console.log('Error occurred: ' + err);
+                    }
+                    console.log("---------------------------------------------------------------")
+                    console.log(`The name of the artist is: ${data.tracks.items[0].artists[0].name}`);
+                    console.log(`The name of the song is: ${data.tracks.items[0].name}`);
+                    console.log(`The preview link to the song is: ${data.tracks.items[0].preview_url}`);
+                    console.log(`The album name of this song is: ${data.tracks.items[0].album.name}`);
+                    console.log("---------------------------------------------------------------")
+
+                    let text = [`The name of the artist is: ${data.tracks.items[0].artists[0].name}`,
+                        `The name of the song is: ${data.tracks.items[0].name}`,
+                        `The preview link to the song is: ${data.tracks.items[0].preview_url}`,
+                        `The album name of this song is: ${data.tracks.items[0].album.name}`
+                    ]
+
+
+                    fs.appendFile("log.txt", text, function(err) {
+
+                        // If an error was experienced we will log it.
+                        if (err) {
+                            console.log(err);
+                        }
+
+                    });
+
+                });
+            }
+            break;
+
+        case "movie-this":
+            if (process.argv.length == 3) {
+                input = "Mr. Nobody";
+                axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy")
+                    .then(function(response) {
+                        console.log("---------------------------------------------------------------")
+                        console.log(`The title is: ${response.data.Title}`);
+                        console.log(`The year is: ${response.data.Year}`);
+                        console.log(`The IMDB rating is: ${response.data.imdbRating}`);
+                        console.log(`The Rotten Tomatoes rating is: ${response.data.Ratings[1].Value}`);
+                        console.log(`The country is: ${response.data.Country}`);
+                        console.log(`The language is: ${response.data.Language}`);
+                        console.log(`The plot is: ${response.data.Plot}`);
+                        console.log(`The actors are: ${response.data.Actors}`);
+                        console.log("---------------------------------------------------------------")
+
+                        let text = [`The title is: ${response.data.Title}`,
+                            `The year is: ${response.data.Year}`,
+                            `The IMDB rating is: ${response.data.imdbRating}`,
+                            `The Rotten Tomatoes rating is: ${response.data.Ratings[1].Value}`,
+                            `The country is: ${response.data.Country}`,
+                            `The language is: ${response.data.Language}`,
+                            `The plot is: ${response.data.Plot}`,
+                            `The actors are: ${response.data.Actors}`
+                        ]
+
+                        fs.appendFile("log.txt", text, function(err) {
+
+                            // If an error was experienced we will log it.
+                            if (err) {
+                                console.log(err);
+                            }
+
+                        });
+
+                    })
+            } else if (process.argv.length > 3) {
+                axios.get("http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy")
+                    .then(function(response) {
+                        console.log("---------------------------------------------------------------")
+                        console.log(`The title is: ${response.data.Title}`);
+                        console.log(`The year is: ${response.data.Year}`);
+                        console.log(`The IMDB rating is: ${response.data.imdbRating}`);
+                        console.log(`The Rotten Tomatoes rating is: ${response.data.Ratings[1].Value}`);
+                        console.log(`The country is: ${response.data.Country}`);
+                        console.log(`The language is: ${response.data.Language}`);
+                        console.log(`The plot is: ${response.data.Plot}`);
+                        console.log(`The actors are: ${response.data.Actors}`);
+                        console.log("---------------------------------------------------------------")
+
+                        let text = [`The title is: ${response.data.Title}`,
+                            `The year is: ${response.data.Year}`,
+                            `The IMDB rating is: ${response.data.imdbRating}`,
+                            `The Rotten Tomatoes rating is: ${response.data.Ratings[1].Value}`,
+                            `The country is: ${response.data.Country}`,
+                            `The language is: ${response.data.Language}`,
+                            `The plot is: ${response.data.Plot}`,
+                            `The actors are: ${response.data.Actors}`
+                        ]
+
+                        fs.appendFile("log.txt", text, function(err) {
+
+                            // If an error was experienced we will log it.
+                            if (err) {
+                                console.log(err);
+                            }
+
+                        });
+
+
+                    })
+            }
+            break;
+
+        case "do-what-it-says":
+            fs.readFile("random.txt", "utf8", function(err, data) {
+                if (err) {
+                    return console.log(err);
+                }
+
+                let dataArr = data.split(",");
+                command = dataArr[0];
+                input = dataArr[1];
+
+                liriBot(); // recursive function call
+            })
+            break;
+
+        default:
+            console.log("invalid command!");
+
+
+    }
+}
+liriBot();
